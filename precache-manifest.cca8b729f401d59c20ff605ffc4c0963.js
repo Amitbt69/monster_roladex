5047b79207b59864ea20224e9049da077b49927a
Amit BT <amitbt@Amit-Mac.local>
2020-05-16 19:41:14 +0300

    Updates

---
+++ precache-manifest.cca8b729f401d59c20ff605ffc4c0963.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12d1f9feba5737e9817f6ae4986f182a",
+    "url": "/monsters_rolodex/index.html"
+  },
+  {
+    "revision": "2cc9303e018bd7104c18",
+    "url": "/monsters_rolodex/static/css/main.8e7fbf0a.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/monsters_rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters_rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cc9303e018bd7104c18",
+    "url": "/monsters_rolodex/static/js/main.684686fb.chunk.js"
+  },
+  {
+    "revision": "929c5ddfd399fdbb9ba6",
+    "url": "/monsters_rolodex/static/js/runtime-main.b3d836e9.js"
+  }
+]);
